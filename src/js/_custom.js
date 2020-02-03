46146c13290d92eb821d6e917ba534792694df13
sokhann <albertsokhann@gmail.com>
2020-02-03 14:39:46 +0300

    preloader

--- src/js/_custom.js
+++ src/js/_custom.js
@@ -1,5 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-
-	// Custom JS
-
-});
+window.onload = function() {
+  document.body.classList.add("loaded_hiding");
+  window.setTimeout(function() {
+    document.body.classList.add("loaded");
+    document.body.classList.remove("loaded_hiding");
+  }, 800);
+};
--- src/js/_libs.js
+++ src/js/_libs.js
@@ -6,70 +6,78 @@
 
 // delete window.IntersectionObserver; // Fallback Testing
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentload", function() {
+  var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+  var lazyBackgrounds = [].slice.call(
+    document.querySelectorAll(".lazy-background")
+  );
+  var lazyBackgroundsData = [].slice.call(
+    document.querySelectorAll("[data-bg]")
+  );
 
-	var lazyImages = [].slice.call(document.querySelectorAll('img.lazy'));
-	var lazyBackgrounds = [].slice.call(document.querySelectorAll('.lazy-background'));
-	var lazyBackgroundsData = [].slice.call(document.querySelectorAll('[data-bg]'));
+  if ("IntersectionObserver" in window) {
+    let lazyImageObserver = new IntersectionObserver(function(
+      entries,
+      observer
+    ) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          let lazyImage = entry.target;
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.srcset = lazyImage.dataset.srcset;
+          lazyImage.classList.remove("lazy");
+          lazyImageObserver.unobserve(lazyImage);
+        }
+      });
+    });
+    lazyImages.forEach(function(lazyImage) {
+      lazyImageObserver.observe(lazyImage);
+    });
 
-	if ('IntersectionObserver' in window) {
+    let lazyBackgroundObserver = new IntersectionObserver(function(
+      entries,
+      observer
+    ) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+          lazyBackgroundObserver.unobserve(entry.target);
+        }
+      });
+    });
+    lazyBackgrounds.forEach(function(lazyBackground) {
+      lazyBackgroundObserver.observe(lazyBackground);
+    });
 
-		let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
-			entries.forEach(function(entry) {
-				if (entry.isIntersecting) {
-					let lazyImage = entry.target;
-					lazyImage.src = lazyImage.dataset.src;
-					lazyImage.srcset = lazyImage.dataset.srcset;
-					lazyImage.classList.remove('lazy');
-					lazyImageObserver.unobserve(lazyImage);
-				}
-			});
-		});
-		lazyImages.forEach(function(lazyImage) {
-			lazyImageObserver.observe(lazyImage);
-		});
+    let lazyBackgroundDataObserver = new IntersectionObserver(function(
+      entries,
+      observer
+    ) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          let lazyBackgroundData = entry.target;
+          lazyBackgroundData.style.backgroundImage =
+            "url(" + lazyBackgroundData.dataset.bg + ")";
+          lazyBackgroundDataObserver.unobserve(lazyBackgroundData);
+        }
+      });
+    });
+    lazyBackgroundsData.forEach(function(lazyBackgroundData) {
+      lazyBackgroundDataObserver.observe(lazyBackgroundData);
+    });
+  } else {
+    // Fallback
 
-	let lazyBackgroundObserver = new IntersectionObserver(function(entries, observer) {
-		entries.forEach(function(entry) {
-			if (entry.isIntersecting) {
-				entry.target.classList.add('visible');
-				lazyBackgroundObserver.unobserve(entry.target);
-			}
-		});
-	});
-	lazyBackgrounds.forEach(function(lazyBackground) {
-		lazyBackgroundObserver.observe(lazyBackground);
-	});
-
-	let lazyBackgroundDataObserver = new IntersectionObserver(function(entries, observer) {
-		entries.forEach(function(entry) {
-			if (entry.isIntersecting) {
-				let lazyBackgroundData = entry.target;
-				lazyBackgroundData.style.backgroundImage = 'url(' + lazyBackgroundData.dataset.bg + ')';
-				lazyBackgroundDataObserver.unobserve(lazyBackgroundData);
-			}
-		});
-	});
-	lazyBackgroundsData.forEach(function(lazyBackgroundData) {
-		lazyBackgroundDataObserver.observe(lazyBackgroundData);
-	});
-
-	} else {
-
-		// Fallback
-
-		lazyImages.forEach(function(lazyImage) {
-			lazyImage.src = lazyImage.dataset.src;
-			lazyImage.srcset = lazyImage.dataset.srcset;
-		});
-		lazyBackgrounds.forEach(function(lazyBackground) {
-			lazyBackground.classList.add('visible');
-		});
-		lazyBackgroundsData.forEach(function(lazyBackgroundData) {
-			lazyBackgroundData.style.backgroundImage = 'url(' + lazyBackgroundData.dataset.bg + ')';
-		});
-
-	}
-
+    lazyImages.forEach(function(lazyImage) {
+      lazyImage.src = lazyImage.dataset.src;
+      lazyImage.srcset = lazyImage.dataset.srcset;
+    });
+    lazyBackgrounds.forEach(function(lazyBackground) {
+      lazyBackground.classList.add("visible");
+    });
+    lazyBackgroundsData.forEach(function(lazyBackgroundData) {
+      lazyBackgroundData.style.backgroundImage =
+        "url(" + lazyBackgroundData.dataset.bg + ")";
+    });
+  }
 });
-
